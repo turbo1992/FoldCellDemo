0ddd8e21c436198b1d870feeb5e6023965b51357
Turbo <zhongshun.zhao@iever.com.cn>
2017-06-27 14:14:13 +0800

    Initial Commit

---
+++ FoldCellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoldCellDemo
+//
+//  Created by Turbo on 2017/6/27.
+//  Copyright © 2017年 Turbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
